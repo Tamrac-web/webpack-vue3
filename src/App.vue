aa0ec4c311ea6b9795b433e544f230a8312998a0
Tamrac <cbzone7@outlook.com>
2023-06-27 23:50:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+import { ref } from "vue";
+
+const msg = ref("我们是移动213的第二组");
+</script>
+
+<template>
+ <h1>{{ msg }}</h1> 
+</template>
+
+<style lang="scss">
+body {
+  background-color: skyblue;
+}
+</style>
